3cc4d85c7866cd6cd3588ab4ee8ba1fe505082ba
mazenfayez <mazenfayez56@gmail.com>
2021-11-17 04:00:52 +0200

    front end

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export class Todo {
-  constructor(public id: number, public desc: string) {}
+  static _id: any;
+  static desc: any;
+  constructor(public _id: number, public desc: string) {}
 }
 
 @Component({
@@ -18,11 +20,41 @@ export class TodosComponent implements OnInit {
   ngOnInit(): void {
     this.getTodo();
   }
+
   getTodo() {
     this.httpClient
       .get<any>('http://localhost:5001/todos')
       .subscribe((todos) => {
         this.todos = todos;
+      });
+  }
+  addTodo(todo: HTMLInputElement) {
+    if (todo.value.length == 0) {
+      alert('please enter a valid input');
+      return;
+    } else {
+      this.httpClient
+        .post<any>('http://localhost:5001/addTodo', {
+          desc: todo.value,
+        })
+        .subscribe(() => {});
+    }
+  }
+  updateTodo(id: Number, newText: HTMLInputElement) {
+    this.httpClient
+      .put<any>(`http://localhost:5001/updateTodo/${id}`, {
+        newText: newText.value,
+        id: id,
+      })
+      .subscribe((todo) => {
+        this.getTodo();
+      });
+  }
+  deleteTodo(id: number) {
+    this.httpClient
+      .delete<any>(`http://localhost:5001/deleteTodo/${id}`)
+      .subscribe((todo) => {
+        this.getTodo();
       });
   }
 }
